c1209be75603477dc435f54b599b1fe92067c2a7
DongLee99 <jhnj741@naver.com>
2021-10-01 16:29:42 +0900

    style: 코드 포맷팅

--- backend/src/main/java/com/minionz/backend/user/controller/UserController.java
+++ backend/src/main/java/com/minionz/backend/user/controller/UserController.java
@@ -7,10 +7,7 @@ import com.minionz.backend.user.controller.dto.UserLogoutResponseDto;
 import com.minionz.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
