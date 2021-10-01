d2e19cb875340b04d71c349c86378b3c44575c9b
woogieme <woogieme@gmail.com>
2021-10-01 20:39:49 +0900

    feat: UserService 구현

--- backend/src/main/java/com/minionz/backend/user/service/UserService.java
+++ backend/src/main/java/com/minionz/backend/user/service/UserService.java
@@ -1,8 +1,6 @@
 package com.minionz.backend.user.service;
 
-import com.minionz.backend.user.controller.dto.UserLoginRequestDto;
-import com.minionz.backend.user.controller.dto.UserLoginResponseDto;
-import com.minionz.backend.user.controller.dto.UserLogoutResponseDto;
+import com.minionz.backend.user.controller.dto.*;
 import com.minionz.backend.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +16,14 @@ public class UserService {
     }
 
     public UserLogoutResponseDto logout(String email) {
+        return null;
+    }
+
+    public UserJoinResponse signUp(UserJoinRequest userJoinRequest) {
+        return null;
+    }
+
+    public UserWithdrawResponse withdraw(String email){
         return null;
     }
 }
