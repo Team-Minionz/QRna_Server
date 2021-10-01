ecdfc330ecff882ad5b8815ebe42bbbbddc57b9c
jeongyuneo <piq2255@gmail.com>
2021-10-02 00:34:47 +0900

    fix: 방문 기록 저장 추가

--- backend/src/main/java/com/minionz/backend/visit/service/VisitService.java
+++ backend/src/main/java/com/minionz/backend/visit/service/VisitService.java
@@ -6,8 +6,11 @@ import com.minionz.backend.user.domain.User;
 import com.minionz.backend.user.domain.UserRepository;
 import com.minionz.backend.visit.controller.dto.CheckInRequestDto;
 import com.minionz.backend.visit.controller.dto.CheckInResponseDto;
+import com.minionz.backend.visit.domain.Visit;
+import com.minionz.backend.visit.domain.VisitRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,10 +18,17 @@ public class VisitService {
 
     private final UserRepository userRepository;
     private final ShopRepository shopRepository;
+    private final VisitRepository visitRepository;
 
+    @Transactional
     public CheckInResponseDto checkIn(CheckInRequestDto checkInRequestDto) {
         User user = userRepository.findByEmail(checkInRequestDto.getUserEmail());
         Shop shop = shopRepository.findByTelNumber(checkInRequestDto.getShopTelNumber());
+        Visit visit = Visit.builder()
+                .user(user)
+                .shop(shop)
+                .build();
+        visitRepository.save(visit);
         return new CheckInResponseDto(user.getId(), shop.getId());
     }
 }
