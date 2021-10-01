7ac2de2c135d8fdadb59e5d9fb855c63ee2b19e2
DongLee99 <jhnj741@naver.com>
2021-10-01 15:16:32 +0900

    Test: 로그인 성공, 실패 테스트

---
+++ backend/src/test/java/com/minionz/backend/user/controller/UserControllerTest.java
@@ -0,0 +1,79 @@
+package com.minionz.backend.user.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.minionz.backend.ApiDocument;
+import com.minionz.backend.common.domain.StatusCode;
+import com.minionz.backend.user.controller.dto.UserLoginRequestDto;
+import com.minionz.backend.user.controller.dto.UserLoginResponseDto;
+import com.minionz.backend.user.domain.User;
+import com.minionz.backend.user.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+class UserControllerTest extends ApiDocument {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void 유저로그인테스트_성공() throws Exception {
+        final UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("email", "password");
+        UserLoginResponseDto userLoginResponseDto = new UserLoginResponseDto(User.builder().email("email").build());
+        willReturn(userLoginResponseDto).given(userService).login(any(UserLoginRequestDto.class));
+        final ResultActions resultActions = 유저_로그인_요청(userLoginRequestDto);
+        유저_로그인_성공(userLoginResponseDto,resultActions);
+    }
+
+    @Test
+    public void 유저로그인테스트_실패() throws Exception {
+        final UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("email1", "password");
+        UserLoginResponseDto userLoginResponseDto = new UserLoginResponseDto(User.builder().email("null").build());
+        userLoginResponseDto.setStatusCode(StatusCode.BAD_REQUEST);
+        //willReturn(userLoginResponseDto).given(userService).login(any(UserLoginRequestDto.class));
+        final ResultActions resultActions = 유저_로그인_요청(userLoginRequestDto);
+        유저_로그인_실패(userLoginResponseDto, resultActions);
+    }
+
+    private void 유저_로그인_실패(UserLoginResponseDto userLoginResponseDto, ResultActions resultActions) throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(userLoginResponseDto)))
+                .andDo(print())
+                .andDo(toDocument("user-login-fail"));
+    }
+
+    private void 유저_로그인_성공(UserLoginResponseDto userLoginResponseDto, ResultActions resultActions) throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(userLoginResponseDto)))
+                .andDo(print())
+                .andDo(toDocument("user-login-success"));
+    }
+
+    private ResultActions 유저_로그인_요청(UserLoginRequestDto userLoginRequestDto) throws Exception{
+        String content = objectMapper.writeValueAsString(userLoginRequestDto);
+        return mockMvc.perform(post("/api/v1/users/login")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON));
+    }
+}
