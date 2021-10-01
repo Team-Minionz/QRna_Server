f03bf2958049b6e031288c2b2f96d1fe0cc86a49
woogieme <woogieme@gmail.com>
2021-10-01 20:40:15 +0900

    style: 코드 포맷팅

--- backend/src/test/java/com/minionz/backend/user/controller/UserControllerTest.java
+++ backend/src/test/java/com/minionz/backend/user/controller/UserControllerTest.java
@@ -3,11 +3,10 @@ package com.minionz.backend.user.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minionz.backend.ApiDocument;
 import com.minionz.backend.common.domain.StatusCode;
-import com.minionz.backend.user.controller.dto.UserLoginRequestDto;
-import com.minionz.backend.user.controller.dto.UserLoginResponseDto;
-import com.minionz.backend.user.controller.dto.UserLogoutResponseDto;
+import com.minionz.backend.user.controller.dto.*;
 import com.minionz.backend.user.domain.User;
 import com.minionz.backend.user.service.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +17,12 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.willReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -114,5 +114,91 @@ class UserControllerTest extends ApiDocument {
         return mockMvc.perform(post("/api/v1/users/login")
                 .content(content)
                 .contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @DisplayName("가입 성공")
+    @Test
+    void user_sign_up_success() throws Exception {
+        UserJoinRequest signUpRequest= new UserJoinRequest("정재욱","wodnr@naver.com","라이언","010-9969-9776","111");
+        UserJoinResponse signUpResponse= new UserJoinResponse(User.builder().name("재욱").build());
+        willReturn(signUpResponse).given(userService).signUp(any(UserJoinRequest.class));
+        //when
+        final ResultActions response = 유저_회원가입_요청(signUpRequest);
+        //then
+        유저_회원가입_성공함(signUpResponse, response);
+    }
+
+    @DisplayName("가입 실패")
+    @Test
+    void user_sign_up_fail() throws Exception {
+        UserJoinRequest signUpRequest= new UserJoinRequest("정재욱","wodnr@naver.com","라이언","010-9969-9776","111");
+        UserJoinResponse signUpResponse= new UserJoinResponse(User.builder().name("null").build());
+        //given
+        signUpResponse.setStatusCode(StatusCode.BAD_REQUEST);
+        //when
+        willReturn(signUpResponse).given(userService).signUp(any(UserJoinRequest.class));
+        final ResultActions response = 유저_회원가입_요청(signUpRequest);
+        //then
+        유저_회원가입_실패(signUpResponse, response);
+    }
+
+    @DisplayName("탈퇴 성공")
+    @Test
+    void user_withdraw_success() throws Exception {
+        final String email = "email";
+        UserWithdrawResponse userWithdrawResponse= new UserWithdrawResponse(User.builder().email("email").build());
+        willReturn(userWithdrawResponse).given(userService).withdraw(email);
+        final ResultActions response = 유저_회원탈퇴_요청(email);
+        유저_회원탈퇴_성공(userWithdrawResponse,response);
+    }
+
+    @DisplayName("탈퇴 실패")
+    @Test
+    void user_withdraw_fail() throws Exception {
+        final String email = "email";
+        UserWithdrawResponse userWithdrawResponse= new UserWithdrawResponse(User.builder().email("email").build());
+        userWithdrawResponse.setStatusCode(StatusCode.BAD_REQUEST);
+        willReturn(userWithdrawResponse).given(userService).withdraw(email);
+        final ResultActions response = 유저_회원탈퇴_요청(email);
+        유저_회원탈퇴_실패(userWithdrawResponse,response);
+    }
+
+    private void 유저_회원탈퇴_실패(UserWithdrawResponse userWithdrawResponse, ResultActions response) throws Exception{
+        response.andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(userWithdrawResponse)))
+                .andDo(print())
+                .andDo(toDocument("user-withdraw-fail"));
+    }
+
+    private ResultActions 유저_회원탈퇴_요청(String email) throws Exception {
+        return mockMvc.perform(delete("/api/v1/users/withdraw/"+email)
+                .contentType(MediaType.APPLICATION_JSON));
+    }
+
+    private ResultActions 유저_회원가입_요청(UserJoinRequest signUpRequest) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(signUpRequest)));
+    }
+
+    private void 유저_회원탈퇴_성공(UserWithdrawResponse userWithdrawResponse, ResultActions response) throws Exception {
+        response.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(toJson(userWithdrawResponse)))
+                .andDo(print())
+                .andDo(toDocument("user-withdraw-success"));
+    }
+
+    private void 유저_회원가입_실패(UserJoinResponse signUpResponse, ResultActions response) throws Exception {
+        response.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(toJson(signUpResponse)))
+                .andDo(print())
+                .andDo(toDocument("user-signup-fail"));
+    }
+
+    private void 유저_회원가입_성공함(UserJoinResponse signUpResponse, ResultActions response) throws Exception {
+        response.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(toJson(signUpResponse)))
+                .andDo(print())
+                .andDo(toDocument("user-signup-success"));
     }
 }
