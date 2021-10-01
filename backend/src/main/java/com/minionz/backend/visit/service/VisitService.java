ffce5a12a3f93ae0fb8f64d041d414ad8b21ffd7
jeongyuneo <piq2255@gmail.com>
2021-10-01 23:01:19 +0900

    feat: Visit 방문 기록 구현

--- backend/src/main/java/com/minionz/backend/visit/service/VisitService.java
+++ backend/src/main/java/com/minionz/backend/visit/service/VisitService.java
@@ -1,13 +1,24 @@
 package com.minionz.backend.visit.service;
 
+import com.minionz.backend.shop.domain.Shop;
+import com.minionz.backend.shop.domain.ShopRepository;
+import com.minionz.backend.user.domain.User;
+import com.minionz.backend.user.domain.UserRepository;
 import com.minionz.backend.visit.controller.dto.CheckInRequestDto;
 import com.minionz.backend.visit.controller.dto.CheckInResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class VisitService {
 
+    private final UserRepository userRepository;
+    private final ShopRepository shopRepository;
+
     public CheckInResponseDto checkIn(CheckInRequestDto checkInRequestDto) {
-        return null;
+        User user = userRepository.findByEmail(checkInRequestDto.getUserEmail());
+        Shop shop = shopRepository.findByTelNumber(checkInRequestDto.getShopTelNumber());
+        return new CheckInResponseDto(user.getId(), shop.getId());
     }
 }
