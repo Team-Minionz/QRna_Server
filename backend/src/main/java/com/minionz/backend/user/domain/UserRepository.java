b2c7a9584efccd98cf84456e8b3034e7384a789e
malpipapi <wodnr8462@naver.com>
2021-10-02 22:02:45 +0900

    refactor: Optional 삭제 및 간편화

--- backend/src/main/java/com/minionz/backend/user/domain/UserRepository.java
+++ backend/src/main/java/com/minionz/backend/user/domain/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    <Optional> User deleteByEmail(String email);
+    User findByEmail(String email);
 }
