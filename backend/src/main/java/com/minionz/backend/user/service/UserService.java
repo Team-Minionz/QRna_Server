0b5070cdba181252cb4f333e95ad7fe1005e8775
malpipapi <woogiepapi@gmail.com>
2021-10-06 23:16:53 +0900

    refactor: 에러 메세지 추가, orElseThrow을 통한 예외발생

--- backend/src/main/java/com/minionz/backend/user/service/UserService.java
+++ backend/src/main/java/com/minionz/backend/user/service/UserService.java
@@ -12,6 +12,7 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
+    private static final String NO_SIGN_USER_MESSAGE = "해당 회원탈퇴에 대한 유저 Email이 존재하지 않습니다.";
     private final UserRepository userRepository;
 
     public UserLoginResponseDto login(UserLoginRequestDto userLoginRequestDto) {
@@ -24,16 +25,17 @@ public class UserService {
 
     @Transactional
     public UserJoinResponse signUp(UserJoinRequest userJoinRequest) {
-        User userIn = userRepository.findByEmail(userJoinRequest.getEmail());
-        User userOut = userJoinRequest.toEntity(userIn);
-        userRepository.save(userOut);
-        return new UserJoinResponse(userRepository.save(userOut));
+        User user = userJoinRequest.toEntity();
+        userRepository.save(user);
+        return new UserJoinResponse(userRepository.save(user));
     }
 
     @Transactional
     public UserWithdrawResponse withdraw(String email) {
-        User user = userRepository.findByEmail(email);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new NoClassDefFoundError(NO_SIGN_USER_MESSAGE));
         userRepository.delete(user);
         return new UserWithdrawResponse(email);
     }
 }
+
