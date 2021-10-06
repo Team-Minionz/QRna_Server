c0167470360c6084d89646f24595dea7534b5834
malpipapi <woogiepapi@gmail.com>
2021-10-06 23:16:03 +0900

    refactor: Optional 추가

--- backend/src/main/java/com/minionz/backend/user/domain/UserRepository.java
+++ backend/src/main/java/com/minionz/backend/user/domain/UserRepository.java
@@ -3,8 +3,11 @@ package com.minionz.backend.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
+
 }
