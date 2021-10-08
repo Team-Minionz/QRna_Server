eb612b750938ae2f85a4a42917336f931921f6ed
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-08 13:51:18 +0900

    refactor: get매핑 dto Setter 추가

--- backend/src/main/java/com/minionz/backend/user/controller/dto/UserRequestDto.java
+++ backend/src/main/java/com/minionz/backend/user/controller/dto/UserRequestDto.java
@@ -3,8 +3,10 @@ package com.minionz.backend.user.controller.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRequestDto {
 
