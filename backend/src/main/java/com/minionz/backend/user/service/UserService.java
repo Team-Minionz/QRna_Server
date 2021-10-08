53a12cad099b5c88e57d8fcdf8655c46cf657952
woogieme <woogieme@gmail.com>
2021-10-08 17:48:31 +0900

    refactor: 회원가입 중복기능 변경

--- backend/src/main/java/com/minionz/backend/user/service/UserService.java
+++ backend/src/main/java/com/minionz/backend/user/service/UserService.java
@@ -1,7 +1,6 @@
 package com.minionz.backend.user.service;
 
 import com.minionz.backend.common.domain.Message;
-import com.minionz.backend.common.exception.BadRequestException;
 import com.minionz.backend.common.exception.NotFoundException;
 import com.minionz.backend.user.controller.dto.UserJoinRequestDto;
 import com.minionz.backend.user.controller.dto.UserLoginRequestDto;
@@ -15,8 +14,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    private static final String SIGN_UP_USER_SUCCESS = "회원가입 성공";
-    private static final String WITHDRAW_USER_SUCCESS = "회원탈퇴 성공";
+    private static final String SIGN_UP_SUCCESS = "회원가입 성공";
+    private static final String WITHDRAW_SUCCESS = "회원탈퇴 성공";
     private static final String NO_SUCH_USER_MESSAGE = "해당 유저가 존재하지 않습니다.";
     private static final String DUPLICATE_USER_MESSAGE = "해당 유저 이메일이 중복입니다";
     private final UserRepository userRepository;
@@ -30,22 +29,18 @@ public class UserService {
     }
 
     public Message signUp(UserJoinRequestDto userJoinRequestDto) {
-        duplicateEmail(userJoinRequestDto);
-        userRepository.save(userJoinRequestDto.toEntity());
-        return new Message(SIGN_UP_USER_SUCCESS);
+        if (userRepository.findByEmail(userJoinRequestDto.getEmail()).orElseGet(null) != null) {
+            throw new NotFoundException(DUPLICATE_USER_MESSAGE);
+        }
+        User user = userRepository.save(userJoinRequestDto.toEntity());
+        userRepository.save(user);
+        return new Message(SIGN_UP_SUCCESS);
     }
 
     public Message withdraw(UserRequestDto userRequestDto) {
         User user = userRepository.findByEmail(userRequestDto.getEmail())
                 .orElseThrow(() -> new NotFoundException(NO_SUCH_USER_MESSAGE));
         userRepository.delete(user);
-        return new Message(WITHDRAW_USER_SUCCESS);
-
-    }
-
-    private void duplicateEmail(UserJoinRequestDto userJoinRequestDto) {
-        if (userRepository.findByEmail(userJoinRequestDto.getEmail()).isPresent()) {
-            throw new BadRequestException(DUPLICATE_USER_MESSAGE);
-        }
+        return new Message(WITHDRAW_SUCCESS);
     }
 }
