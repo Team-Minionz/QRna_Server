50eed2a6271039c9bacef5a99083846270ac89df
woogieme <woogieme@gmail.com>
2021-10-08 23:03:21 +0900

    refactor: Setter 어노테이션 삭제

--- backend/src/main/java/com/minionz/backend/common/domain/Address.java
+++ backend/src/main/java/com/minionz/backend/common/domain/Address.java
@@ -3,13 +3,11 @@ package com.minionz.backend.common.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-@Setter
 @NoArgsConstructor
 public class Address {
 
