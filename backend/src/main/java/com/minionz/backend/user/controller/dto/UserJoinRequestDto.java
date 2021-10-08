ce8865ae05cb77528690e9806a1adb9e4fce71fd
woogieme <woogieme@gmail.com>
2021-10-08 17:47:52 +0900

    fix: 회원가입 Adddress 추가

--- backend/src/main/java/com/minionz/backend/user/controller/dto/UserJoinRequestDto.java
+++ backend/src/main/java/com/minionz/backend/user/controller/dto/UserJoinRequestDto.java
@@ -1,5 +1,6 @@
 package com.minionz.backend.user.controller.dto;
 
+import com.minionz.backend.common.domain.Address;
 import com.minionz.backend.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,23 +17,26 @@ public class UserJoinRequestDto {
     private String nickName;
     private String telNumber;
     private String password;
+    private Address address;
 
     @Builder
-    public UserJoinRequestDto(String name, String email, String nickName, String telNumber, String password) {
+    public UserJoinRequestDto(String name, String email, String nickName, String telNumber, String password, Address address) {
         this.name = name;
         this.email = email;
         this.nickName = nickName;
         this.telNumber = telNumber;
         this.password = password;
+        this.address = address;
     }
 
     public User toEntity() {
         return User.builder()
-                .name(name)
-                .nickName(nickName)
-                .password(password)
-                .email(email)
-                .telNumber(telNumber)
+                .name(this.name)
+                .nickName(this.nickName)
+                .password(this.password)
+                .email(this.email)
+                .telNumber(this.telNumber)
+                .address(this.address)
                 .build();
     }
 }
