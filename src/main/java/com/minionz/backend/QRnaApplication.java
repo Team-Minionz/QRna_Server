8f179ae868f653ef51f5435a097cc87aaecfc3ba
jeongyuneo <piq2255@gmail.com>
2021-10-13 18:14:46 +0900

    style: 메인 클래스 이름 변경

--- src/main/java/com/minionz/backend/QRnaApplication.java
+++ src/main/java/com/minionz/backend/QRnaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class QRnaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(QRnaApplication.class, args);
     }
 }
