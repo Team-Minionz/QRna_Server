e348016659aaa2ba9dc5a135857c7976fe40743a
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-17 13:22:23 +0900

    style: 코드 포멧팅

--- src/test/java/com/minionz/backend/shop/controller/ShopControllerTest.java
+++ src/test/java/com/minionz/backend/shop/controller/ShopControllerTest.java
@@ -41,122 +41,122 @@ class ShopControllerTest extends ApiDocument {
 
     @DisplayName("상점 등록 성공")
     @Test
-    void 상점_등록_성공() throws Exception {
+    void 상점등록_성공() throws Exception {
         Address address = new Address("인천시", "부평구", "산곡동");
         ShopRequestDto shopRequestDto = new ShopRequestDto("맥도날드", address, "032-123-1234");
         Message message = new Message("Shop 등록 성공");
         willReturn(message).given(shopService).save(any(ShopRequestDto.class));
-        ResultActions resultActions = 상점_등록_요청(shopRequestDto);
-        상점_등록_요청_성공(message, resultActions);
+        ResultActions resultActions = 상점등록_요청(shopRequestDto);
+        상점등록요청_성공(message, resultActions);
     }
 
     @DisplayName("상점 등록 실패")
     @Test
-    void 상점_등록_실패() throws Exception {
+    void 상점등록_실패() throws Exception {
         Address address = new Address("인천시", "부평구", "산곡동");
         ShopRequestDto shopRequestDto = new ShopRequestDto("맥도날드", address, "032-123-1234");
         Message message = new Message("Shop 등록 실패");
         willThrow(new BadRequestException("Shop 등록 실패")).given(shopService).save(any(ShopRequestDto.class));
-        ResultActions resultActions = 상점_등록_요청(shopRequestDto);
-        상점_등록_요청_실패(message, resultActions);
+        ResultActions resultActions = 상점등록_요청(shopRequestDto);
+        상점등록요청_실패(message, resultActions);
     }
 
     @DisplayName("상점 update 성공")
     @Test
-    void 상점_수정_성공() throws Exception {
+    void 상점수정_성공() throws Exception {
         Long id = 1L;
         Address address = new Address("인천시", "부평구", "산곡동");
         ShopRequestDto shopRequestDto = new ShopRequestDto("맥도날드", address, "032-123-1234");
         Message message = new Message("Shop 수정 성공");
         willReturn(message).given(shopService).update(any(Long.class) ,any(ShopRequestDto.class));
-        ResultActions resultActions = 상점_수정_요청(id, shopRequestDto);
-        상점_수정_요청_성공(resultActions);
+        ResultActions resultActions = 상점수정_요청(id, shopRequestDto);
+        상점수정요청_성공(resultActions);
     }
 
     @DisplayName("상점 update 실패")
     @Test
-    void 상점_수정_실패() throws Exception {
+    void 상점수정_실패() throws Exception {
         Long id = 1L;
         Address address = new Address("인천시", "부평구", "산곡동");
         ShopRequestDto shopRequestDto = new ShopRequestDto("맥도날드", address, "032-123-1234");
         Message message = new Message("Shop 수정 실패");
         willThrow(new NotFoundException("Shop 수정 실패")).given(shopService).update(any(Long.class), any(ShopRequestDto.class));
-        ResultActions resultActions = 상점_수정_요청(id, shopRequestDto);
-        상점_수정_요청_실패(message, resultActions);
+        ResultActions resultActions = 상점수정_요청(id, shopRequestDto);
+        상점수정요청_실패(message, resultActions);
     }
 
     @DisplayName("상점 delete 성공")
     @Test
-    void 상점_삭제_성공() throws Exception {
+    void 상점삭제_성공() throws Exception {
         Long id = 1L;
         Message message = new Message("Shop 삭제 성공");
         willReturn(message).given(shopService).delete(any(Long.class));
-        ResultActions resultActions = 상점_삭제_요청(id);
-        상점_삭제_요청_성공(resultActions);
+        ResultActions resultActions = 상점삭제_요청(id);
+        상점삭제요청_성공(resultActions);
     }
 
     @DisplayName("상점 delete 실패")
     @Test
-    void 상점_삭제_실패() throws Exception {
+    void 상점삭제_실패() throws Exception {
         Long id = 1L;
         Message message = new Message("Shop 삭제 실패");
         willThrow(new NotFoundException("Shop 삭제 실패")).given(shopService).delete(any(Long.class));
-        ResultActions resultActions = 상점_삭제_요청(id);
-        상점_삭제_요청_실패(message, resultActions);
+        ResultActions resultActions = 상점삭제_요청(id);
+        상점삭제요청_실패(message, resultActions);
     }
 
-    private void 상점_삭제_요청_실패(Message message, ResultActions resultActions) throws Exception {
+    private void 상점삭제요청_실패(Message message, ResultActions resultActions) throws Exception {
         resultActions.andExpect(status().isNotFound())
                 .andExpect(content().json(toJson(message)))
                 .andDo(print())
                 .andDo(toDocument("shop-delete-fail"));
     }
 
-    private void 상점_수정_요청_실패(Message message, ResultActions resultActions) throws Exception {
+    private void 상점수정요청_실패(Message message, ResultActions resultActions) throws Exception {
         resultActions.andExpect(status().isNotFound())
                 .andExpect(content().json(toJson(message)))
                 .andDo(print())
                 .andDo(toDocument("shop-update-fail"));
     }
 
-    private void 상점_등록_요청_실패(Message message, ResultActions resultActions) throws Exception {
+    private void 상점등록요청_실패(Message message, ResultActions resultActions) throws Exception {
         resultActions.andExpect(status().isBadRequest())
                 .andExpect(content().json(toJson(message)))
                 .andDo(print())
                 .andDo(toDocument("shop-save-fail"));
     }
 
-    private void 상점_등록_요청_성공(Message message, ResultActions resultActions) throws Exception {
+    private void 상점등록요청_성공(Message message, ResultActions resultActions) throws Exception {
         resultActions.andExpect(status().isCreated())
                 .andExpect(content().json(toJson(message)))
                 .andDo(print())
                 .andDo(toDocument("shop-save-success"));
     }
 
-    private ResultActions 상점_등록_요청(ShopRequestDto shopRequestDto) throws Exception {
+    private ResultActions 상점등록_요청(ShopRequestDto shopRequestDto) throws Exception {
         return mockMvc.perform(post("/api/v1/shops")
                 .content(objectMapper.writeValueAsString(shopRequestDto))
                 .contentType(MediaType.APPLICATION_JSON));
     }
-    private void 상점_수정_요청_성공(ResultActions resultActions) throws Exception {
+    private void 상점수정요청_성공(ResultActions resultActions) throws Exception {
         resultActions.andExpect(status().isNoContent())
                 .andDo(print())
                 .andDo(toDocument("shop-update-success"));
     }
 
-    private ResultActions 상점_수정_요청(Long id, ShopRequestDto shopRequestDto) throws Exception {
+    private ResultActions 상점수정_요청(Long id, ShopRequestDto shopRequestDto) throws Exception {
         return mockMvc.perform(put("/api/v1/shops/" + id)
                 .content(objectMapper.writeValueAsString(shopRequestDto))
                 .contentType(MediaType.APPLICATION_JSON));
     }
 
-    private void 상점_삭제_요청_성공(ResultActions resultActions) throws Exception {
+    private void 상점삭제요청_성공(ResultActions resultActions) throws Exception {
         resultActions.andExpect(status().isNoContent())
                 .andDo(print())
                 .andDo(toDocument("shop-delete-success"));
     }
 
-    private ResultActions 상점_삭제_요청(Long id) throws Exception {
+    private ResultActions 상점삭제_요청(Long id) throws Exception {
         return mockMvc.perform(delete("/api/v1/shops/" + id)
                 .contentType(MediaType.APPLICATION_JSON));
     }
