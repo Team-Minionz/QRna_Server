d65e47055a01df16487a64004bbf95b331659939
jeongyuneo <piq2255@gmail.com>
2021-10-18 20:39:38 +0900

    fix: @Service, @Transactional 어노테이션 추가

--- src/main/java/com/minionz/backend/shop/service/ShopService.java
+++ src/main/java/com/minionz/backend/shop/service/ShopService.java
@@ -4,17 +4,23 @@ import com.minionz.backend.common.domain.Message;
 import com.minionz.backend.shop.controller.dto.ShopSaveRequestDto;
 import com.minionz.backend.shop.domain.Shop;
 import com.minionz.backend.shop.domain.ShopRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
+@Service
 public class ShopService {
 
     private static final String SAVE_SUCCESS_MESSAGE = "가게 등록 성공";
 
-    private ShopRepository shopRepository;
+    private final ShopRepository shopRepository;
 
+    @Transactional
     public Message save(ShopSaveRequestDto shopSaveRequestDto) {
         Shop shop = shopSaveRequestDto.toEntity();
-        shopRepository.save(shop);
         shop.mapShopWithTable();
+        shopRepository.save(shop);
         return new Message(SAVE_SUCCESS_MESSAGE);
     }
 }
