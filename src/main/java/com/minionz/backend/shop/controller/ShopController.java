bd191d9672a00bdee3a7889c0ea7ef07bb754fa0
jeongyuneo <piq2255@gmail.com>
2021-10-19 00:48:17 +0900

    fix: ShopController 필요생성자 어노테이션 추가 및 shopService 접근 지정자 설정

--- src/main/java/com/minionz/backend/shop/controller/ShopController.java
+++ src/main/java/com/minionz/backend/shop/controller/ShopController.java
@@ -3,6 +3,7 @@ package com.minionz.backend.shop.controller;
 import com.minionz.backend.common.domain.Message;
 import com.minionz.backend.shop.controller.dto.ShopSaveRequestDto;
 import com.minionz.backend.shop.service.ShopService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/shops")
 public class ShopController {
 
-    private ShopService shopService;
+    private final ShopService shopService;
 
     @PostMapping
     public Message save(@RequestBody ShopSaveRequestDto shopSaveRequestDto) {
