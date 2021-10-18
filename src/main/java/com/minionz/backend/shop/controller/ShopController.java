49fc1dacd8414f821c113b0770b968005b0be29b
jeongyuneo <piq2255@gmail.com>
2021-10-18 16:05:04 +0900

    feat: ShopController 추가

---
+++ src/main/java/com/minionz/backend/shop/controller/ShopController.java
@@ -0,0 +1,25 @@
+package com.minionz.backend.shop.controller;
+
+import com.minionz.backend.common.domain.Message;
+import com.minionz.backend.shop.controller.dto.ShopSaveRequestDto;
+import com.minionz.backend.shop.service.ShopService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/shops")
+public class ShopController {
+
+    private ShopService shopService;
+
+    @PostMapping
+    public Message save(@RequestBody ShopSaveRequestDto shopSaveRequestDto) {
+        Message message = shopService.save(shopSaveRequestDto);
+        log.info(message.getMessage());
+        return message;
+    }
+}
