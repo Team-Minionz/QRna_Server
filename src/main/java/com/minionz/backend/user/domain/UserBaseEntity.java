d0ecaa071f8855b9360de88f5ebc4807db621529
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-20 22:02:26 +0900

    feat: Owner와 User의 코드 중복을 줄이기 위해 UserBaseEntity 구현

---
+++ src/main/java/com/minionz/backend/user/domain/UserBaseEntity.java
@@ -0,0 +1,36 @@
+package com.minionz.backend.user.domain;
+
+import com.minionz.backend.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+public class UserBaseEntity extends BaseEntity {
+
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String telNumber;
+
+    public UserBaseEntity(Long id, LocalDateTime createdDate, LocalDateTime modifiedDate, String name, String email, String password, String telNumber) {
+        super(id, createdDate, modifiedDate);
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.telNumber = telNumber;
+    }
+}
