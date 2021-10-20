1df611e229c5451ab0647e526820a1aa7a407204
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-20 19:49:05 +0900

    feat: configure를 통해 모든 요청 허용

--- src/main/java/com/minionz/backend/auth/SecurityConfig.java
+++ src/main/java/com/minionz/backend/auth/SecurityConfig.java
@@ -2,14 +2,25 @@ package com.minionz.backend.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().headers().frameOptions().sameOrigin()
+                .and().csrf().disable();
+    }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
