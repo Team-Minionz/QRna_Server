d1479ad1b64b6c0f68dbc1e8507674a4a670bc87
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-20 22:04:00 +0900

    feat: Shop과 Owner 일대다 맵핑

--- src/main/java/com/minionz/backend/shop/domain/Shop.java
+++ src/main/java/com/minionz/backend/shop/domain/Shop.java
@@ -3,6 +3,7 @@ package com.minionz.backend.shop.domain;
 import com.minionz.backend.common.domain.Address;
 import com.minionz.backend.common.domain.BaseEntity;
 import com.minionz.backend.shop.controller.dto.ShopRequestDto;
+import com.minionz.backend.user.domain.Owner;
 import com.minionz.backend.visit.domain.Visit;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -29,15 +30,20 @@ public class Shop extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String telNumber;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
     @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
     private List<Visit> visitList = new ArrayList<>();
 
     @Builder
-    public Shop(Long id, LocalDateTime createDate, LocalDateTime lastModifiedDate, String name, Address address, String telNumber) {
+    public Shop(Long id, LocalDateTime createDate, LocalDateTime lastModifiedDate,Owner owner, String name, Address address, String telNumber) {
         super(id, createDate, lastModifiedDate);
         this.name = name;
         this.address = address;
         this.telNumber = telNumber;
+        this.owner = owner;
     }
 
     public void update(ShopRequestDto shopRequestDto) {
