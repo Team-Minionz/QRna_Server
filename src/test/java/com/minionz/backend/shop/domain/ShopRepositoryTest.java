81f1c1064b01bd8c627553a777a12c2e936dbc78
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-23 21:24:26 +0900

    test: owner save 추가

--- src/test/java/com/minionz/backend/shop/domain/ShopRepositoryTest.java
+++ src/test/java/com/minionz/backend/shop/domain/ShopRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.minionz.backend.shop.domain;
 
 import com.minionz.backend.common.domain.Address;
+import com.minionz.backend.user.domain.Owner;
+import com.minionz.backend.user.domain.OwnerRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,9 @@ public class ShopRepositoryTest {
     @Autowired
     private ShopRepository shopRepository;
 
+    @Autowired
+    private OwnerRepository ownerRepository;
+
     @AfterEach
     void cleanUp() {
         shopRepository.deleteAll();
@@ -37,11 +42,19 @@ public class ShopRepositoryTest {
         list.add(ShopTable.builder().maxUser(4).build());
         list.add(ShopTable.builder().maxUser(4).build());
         Address address = Address.builder().zipcode("111-222").street("구월동").city("인천시 남동구").build();
+        Owner owner = Owner.builder()
+                .name("주인")
+                .email("jhnj841@naba.com")
+                .password("123")
+                .telNumber("123123")
+                .build();
+        ownerRepository.save(owner);
         Shop shop = Shop.builder()
                 .name("name")
                 .telNumber("032-888-8888")
                 .address(address)
                 .tableList(list)
+                .owner(owner)
                 .build();
         shop.mapShopWithTable();
         // when
