1d8cafd29a90504aaab358f1a5a17f45409dd0bf
malpipapi <wodnr8462@naver.com>
2021-10-29 12:25:09 +0900

    refactor: 불필요한 user 정보 삭제

--- src/main/java/com/minionz/backend/user/controller/dto/UserVisitResponse.java
+++ src/main/java/com/minionz/backend/user/controller/dto/UserVisitResponse.java
@@ -2,8 +2,6 @@ package com.minionz.backend.user.controller.dto;
 
 import com.minionz.backend.common.domain.Address;
 import com.minionz.backend.shop.domain.Shop;
-import com.minionz.backend.user.domain.User;
-import com.minionz.backend.user.domain.UserBaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,20 +9,14 @@ import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
-public class UserVisitResponse extends UserBaseEntity {
+public class UserVisitResponse {
 
-    private String userName;
-    private Address userAddress;
-    private String userTelNumber;
     private String shopName;
     private Address shopAddress;
     private String shopTelNumber;
     private LocalDateTime visitedDate;
 
-    public UserVisitResponse(User user, Shop shop, LocalDateTime visitedDate) {
-        this.userName = user.getName();
-        this.userAddress = user.getAddress();
-        this.userTelNumber =user. getTelNumber();
+    public UserVisitResponse(Shop shop, LocalDateTime visitedDate) {
         this.shopName = shop.getName();
         this.shopAddress = shop.getAddress();
         this.shopTelNumber = shop.getTelNumber();
