ce2076442ae985efe3c85b3b0d37b36846aba5e0
malpipapi <wodnr8462@naver.com>
2021-10-29 12:23:25 +0900

    refactor: 마이페이지안에 상세정보

--- src/main/java/com/minionz/backend/user/controller/dto/UserPageResponseDto.java
+++ src/main/java/com/minionz/backend/user/controller/dto/UserPageResponseDto.java
@@ -6,6 +6,8 @@ import com.minionz.backend.user.domain.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class UserPageResponseDto {
@@ -13,11 +15,13 @@ public class UserPageResponseDto {
     private String nickname;
     private String telNumber;
     private Address address;
+    private List<UserVisitResponse> userVisitResponseList;
 
-    public UserPageResponseDto(User user) {
+    public UserPageResponseDto(User user, List<UserVisitResponse> userVisitResponseList) {
         this.nickname = user.getNickName();
         this.address = user.getAddress();
         this.telNumber = user.getTelNumber();
+        this.userVisitResponseList = userVisitResponseList;
     }
 
     public UserPageResponseDto(Owner owner) {
