5d7d97b61a3ff4766e0f26b2edb3bd6836378702
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-11-02 21:17:09 +0900

    feat: CommonShopResponseDto 샵을 이용한 생성자 추가

--- src/main/java/com/minionz/backend/shop/controller/dto/CommonShopResponseDto.java
+++ src/main/java/com/minionz/backend/shop/controller/dto/CommonShopResponseDto.java
@@ -3,6 +3,7 @@ package com.minionz.backend.shop.controller.dto;
 import com.minionz.backend.common.domain.Address;
 import com.minionz.backend.common.dto.AddressDto;
 import com.minionz.backend.shop.domain.CongestionStatus;
+import com.minionz.backend.shop.domain.Shop;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,5 +25,14 @@ public class CommonShopResponseDto {
         this.congestionStatus = congestionStatus;
         this.numberOfTables = numberOfTables;
         this.useTables = useTables;
+    }
+
+    public CommonShopResponseDto(Shop shop) {
+        this.id = shop.getId();
+        this.name = shop.getName();
+        this.address = new AddressDto(shop.getAddress());
+        this.congestionStatus = shop.getCongestionStatus();
+        this.numberOfTables = shop.getNumberOfTables();
+        this.useTables = shop.getNumberOfUsingTables();
     }
 }
