c55142a181d6a2002de060dda0bbe5e2ea1a76c5
woogieme <woogieme@gmail.com>
2021-11-05 13:46:50 +0900

    fix: 샵 상세보기 ServiceTest 생성

--- src/test/java/com/minionz/backend/shop/service/ShopServiceTest.java
+++ src/test/java/com/minionz/backend/shop/service/ShopServiceTest.java
@@ -2,6 +2,7 @@ package com.minionz.backend.shop.service;
 
 import com.minionz.backend.common.domain.Address;
 import com.minionz.backend.common.exception.NotFoundException;
+import com.minionz.backend.shop.controller.dto.ShopDetailResponseDto;
 import com.minionz.backend.shop.controller.dto.ShopRequestDto;
 import com.minionz.backend.shop.controller.dto.ShopSaveResponseDto;
 import com.minionz.backend.shop.controller.dto.ShopTableRequestDto;
@@ -9,6 +10,11 @@ import com.minionz.backend.shop.domain.Shop;
 import com.minionz.backend.shop.domain.ShopRepository;
 import com.minionz.backend.user.domain.Owner;
 import com.minionz.backend.user.domain.OwnerRepository;
+import com.minionz.backend.user.domain.User;
+import com.minionz.backend.user.domain.UserRepository;
+import com.minionz.backend.user.service.UserService;
+import com.minionz.backend.visit.controller.dto.CheckInRequestDto;
+import com.minionz.backend.visit.service.VisitService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,6 +40,15 @@ public class ShopServiceTest {
 
     @Autowired
     private OwnerRepository ownerRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private VisitService visitService;
 
     @AfterEach
     void cleanUp() {
@@ -64,5 +79,42 @@ public class ShopServiceTest {
                 .orElseThrow(() -> new NotFoundException("매장 등록 실패"));
         // then
         assertThat(shopSaveResponseDto.getId()).isEqualTo(shop.getId());
+    }
+
+    @DisplayName("Shop 상세보기 매장(공통) 테스트")
+    @Test
+    public void viewShopDetailTest() {
+        // given
+        List<ShopTableRequestDto> list = new ArrayList<>();
+        list.add(new ShopTableRequestDto(2));
+        list.add(new ShopTableRequestDto(4));
+        list.add(new ShopTableRequestDto(4));
+        Address address = Address.builder().zipcode("111-222").street("구월동").city("인천시 남동구").build();
+        Address userAddress = Address.builder().zipcode("111-222").street("구월동").city("인천시 남동구").build();
+        Owner owner = Owner.builder()
+                .name("주인")
+                .email("jhnj841@naba.com")
+                .password("123")
+                .telNumber("010-1234-1234")
+                .build();
+        Owner savedOwner = ownerRepository.save(owner);
+        User user = User.builder()
+                .name("정재욱")
+                .password("12345")
+                .telNumber("010-9969-9776")
+                .address(userAddress)
+                .email("wodnr8462@naver.com")
+                .nickName("김해룡축구장")
+                .build();
+        userRepository.save(user);
+        //when
+        ShopRequestDto shopRequestDto = new ShopRequestDto("name", address, "032-888-8888", list, savedOwner.getId());
+        ShopSaveResponseDto shopSaveResponseDto = shopService.save(shopRequestDto);
+        CheckInRequestDto checkInRequestDto = new CheckInRequestDto(user.getId(), shopSaveResponseDto.getId());
+        visitService.checkIn(checkInRequestDto);
+        ShopDetailResponseDto shopDetailResponseDto = shopService.viewDetail(user.getId(), shopSaveResponseDto.getId());
+        // then
+        assertThat(shopDetailResponseDto.getMaxUser()).isEqualTo(10);
+        assertThat(shopDetailResponseDto.getUseUser()).isEqualTo(1);
     }
 }
