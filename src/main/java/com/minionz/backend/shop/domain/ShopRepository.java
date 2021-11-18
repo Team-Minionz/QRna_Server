b395e3b4fd9bf758a1c23008b6ab601b93584eac
jeongyuneo <piq2255@gmail.com>
2021-11-18 12:54:34 +0900

    refactor: ShopRepository 이름,지역 검색 Query 제거

--- src/main/java/com/minionz/backend/shop/domain/ShopRepository.java
+++ src/main/java/com/minionz/backend/shop/domain/ShopRepository.java
@@ -1,7 +1,6 @@
 package com.minionz.backend.shop.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,6 +19,5 @@ public interface ShopRepository extends JpaRepository<Shop, Long> {
 
     List<Shop> findByNameContains(String name);
 
-    @Query(value = "select * from shop where shop_name like %:name% and city like %:city%", nativeQuery = true)
-    List<Shop> findByNameContainsAndAddress_CityContains(@Param("name") String name, @Param("city") String city);
+    List<Shop> findByNameContainsAndAddress_CityContains(String name, String city);
 }
