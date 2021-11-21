271349b6904179d062e606847f37ae97b79843b5
jeongyuneo <piq2255@gmail.com>
2021-11-21 17:16:24 +0900

    refactor: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot-study-project-qrna | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-         -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/qrna/application-real-db.properties \
+         -Dspring.config.location=classpath:/application-real.properties, /home/ec2-user/qrna/application-real-db.properties \
          -Dspring.profiles.active=real \
          $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
